6337b44cfe2e9490d67ff020ea70c9ea55b1bf29
bryazginkos <bryazginkos@yahoo.com>
2015-08-09 16:45:41 +0600

    build fix

--- src/main/java/ru/kosdev/gwtwebsocket/client/WSConfigurationValidator.java
+++ src/main/java/ru/kosdev/gwtwebsocket/client/WSConfigurationValidator.java
@@ -7,7 +7,6 @@ public class WSConfigurationValidator {
 
     public static void validate(WSConfiguration configuration) {
         if (configuration == null) throw new IllegalArgumentException("Websocket configuration is null");
-        if (configuration.getCallback() == null) throw new IllegalArgumentException("Websocket Callback is null");
         if (configuration.getGClass() == null) throw new IllegalArgumentException("Websocket G class is null. Use G.getClass");
         if (configuration.getSClass() == null) throw new IllegalArgumentException("Websocket S class is null. Use S.getClass");
         if (configuration.getUrl() == null) throw new IllegalArgumentException("Websocket URL is null");
